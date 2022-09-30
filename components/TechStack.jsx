58570b839e5fc75f64212d337b68239df6ba579f
Mohammed Safvan <mohammedsafvannk@gmail.com>
2022-09-30 10:46:10 +0530

    techStack completed

---
+++ components/TechStack.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const TechStack = ({ url, imageUrl }) => {
+  return (
+    <a href={url} target="_blank" rel="noreferrer">
+      <div className="w-2"></div>
+      <img
+        src={imageUrl}
+        width="36"
+        height="36"
+        alt="Flutter"
+        className="w-10 md:w-14"
+      />
+    </a>
+  );
+};
+
+export default TechStack;
---
+++ helpers/data.js
@@ -0,0 +1,74 @@
+export const techStacks = [
+  {
+    name: "Flutter",
+    url: "https://flutter.dev/",
+    imageUrl:
+      "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/flutter-colored.svg",
+  },
+  {
+    name: "HTML",
+    url: "https://developer.mozilla.org/en-US/docs/Glossary/HTML5",
+    imageUrl:
+      "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/html5-colored.svg",
+  },
+  {
+    name: "CSS",
+    url: "https://www.w3.org/TR/CSS/#css",
+    imageUrl:
+      "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/css3-colored.svg",
+  },
+  {
+    name: "JavaScript",
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    imageUrl:
+      "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/javascript-colored.svg",
+  },
+  {
+    name: "React",
+    url: "https://reactjs.org/",
+    imageUrl:
+      "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/react-colored.svg",
+  },
+  {
+    name: "NodeJS",
+    url: "https://nodejs.org/en/",
+    imageUrl:
+      "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/nodejs-colored.svg",
+  },
+  {
+    name: "Express",
+    url: "https://expressjs.com/",
+    imageUrl:
+      "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/express-colored.svg",
+  },
+  {
+    name: "MySQL",
+    url: "https://www.mysql.com/",
+    imageUrl:
+      "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/mysql-colored.svg",
+  },
+  {
+    name: "MongoDB",
+    url: "https://www.mongodb.com/",
+    imageUrl:
+      "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/mongodb-colored.svg",
+  },
+  {
+    name: "FireBase",
+    url: "https://firebase.google.com/",
+    imageUrl:
+      "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/firebase-colored.svg",
+  },
+];
+
+export const projects = [
+  {
+    name: "Todo Firebase",
+    stack: "Flutter, Firebase",
+    image: "../public/todo_firebase.png",
+    description:
+      "A todo app with firebase cloud firestore and authentication(Phone, Google, Email) with persistant storage",
+    url: "https://github.com/mohammedsafvan/todo_firebase",
+  },
+  { name: "", stack: "", description: "", url: "" },
+];
